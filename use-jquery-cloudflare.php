749e08432d9aa2c3645684a272797940ab37bff0
luciole135 <luciole135@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-18 10:13:52 +0000

    nouvelle version
    git-svn-id: https://plugins.svn.wordpress.org/use-jquery-cloudflare/trunk@1009505 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ use-jquery-cloudflare.php
@@ -0,0 +1,22 @@
+<?php
+ /*
+Plugin Name: Use jQuery Cloudflare
+Plugin URI: http://additifstabac.free.fr/index.php/use-jquery-cloudflare/
+Donate link: additifstabac@free.fr
+Description: Charge les bibliothèques open source jQuery et jQuery-migrate depuis le CDN de Cloudflare
+Author: luciole135
+Author URI: http://additifstabac.free.fr
+Version: 1.0.2
+*/
+function modify_jquery() {global $wp_scripts;
+	if (!is_admin()) {
+		$jquery_ver = $wp_scripts->registered['jquery']->ver;
+		$jquery_migrate_ver = $wp_scripts->registered['jquery-migrate']->ver;
+		wp_deregister_script('jquery');
+		wp_deregister_script('jquery-migrate');
+		wp_enqueue_script('jquery', 'http://cdnjs.cloudflare.com/ajax/libs/jquery/'.$jquery_ver.'/jquery.min.js"', false, null,true);
+		wp_enqueue_script('jquery-migrate', 'http://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/'.$jquery_migrate_ver.'/jquery-migrate.min.js', false, null,true);
+		}
+}
+add_action('wp_enqueue_scripts', 'modify_jquery',9999);
+?>
